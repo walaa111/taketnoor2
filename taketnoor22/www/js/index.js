e04469712497ea7942327d71c6cccec7f1f4a28d
walaa111 <walaagamil18@gmail.com>
2017-05-04 14:40:07 +0200

    Update index.js

--- taketnoor22/www/js/index.js
+++ taketnoor22/www/js/index.js
@@ -24,12 +24,42 @@ var ref
           ref.hide();
           ref.addEventListener('exit', inappcallback);
           ref.addEventListener('loadstop', startcallback);
-
+ ref.addEventListener('admob', adSetter);
    },1000)
    document.addEventListener("backbutton", function (e) {
            confirm('Are you sure you want to save this thing into the database?')
        }, false );
  });
+
+function adSetter(){
+alert(navigator.userAgent);
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-6869992474017983/9375997553',
+        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-6869992474017983/4806197152',
+        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-6869992474017983/8878394753',
+        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+    };
+}
+
+if(AdMob) AdMob.createBanner( {
+    isTesting:true, //Remove this Before publishing your app
+    adId:admobid.banner, 
+    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+    autoShow:true} );
+
+}
+
  function inappcallback(e){
    navigator.app.exitApp();
  }
